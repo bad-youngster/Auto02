44ef9d8e600d4396dadf2dbde0b0980bd6417849
wayne <should829@gmail.com>
2018-09-05 18:52:08 +0800

    change makerdirs to mysql tables

---
+++ TEST_DEMO/Path_directory.py
@@ -0,0 +1,32 @@
+#coding:utf-8
+import os
+import time
+
+from TEST_DEMO.insert_sql import cursor, sql1, db
+
+nowtime = time.strftime('%Y-%m-%d',time.localtime(time.time()))
+
+
+# def created(self):
+#     project_name = ["lypt", "agent"]
+#     for project_path in project_name:
+#         newfilepath = ('/tmp/%s/%s' % (project_path, nowtime))
+#         if not os.path.exists(newfilepath):
+#             os.makedirs(newfilepath)
+#
+#
+# createdirectory = created(nowtime)
+# print(createdirectory)
+project_name = ["lypt", "agent"]
+for project_path in project_name:
+    newfilepath = ('/tmp/%s/%s' % (project_path, nowtime))
+    if not os.path.exists(newfilepath):
+        os.makedirs(newfilepath)
+        try:
+            # cursor.execute(sql)
+            cursor.execute(sql1)
+            db.commit()
+        except Exception as e:
+            db.rollback()
+            print(e)
+db.close()
---
+++ TEST_DEMO/test10.py
@@ -0,0 +1,39 @@
+#coding:utf-8
+import datetime
+import os
+import pwd
+import time
+
+import pymysql
+
+host_name = "pymysql"
+host_ip = "192.168.56.102"
+nowtime = datetime.datetime.now()
+create_user = pwd.getpwuid(os.getegid())[0]
+
+nowtime1 = time.strftime('%Y-%m-%d',time.localtime(time.time()))
+project_name = ["lypt", "agent"]
+
+
+
+
+db = pymysql.connect("192.168.56.102","root","123456","pymysql")
+cursor = db.cursor()
+for project_path in project_name:
+    newfilepath = ('/tmp/%s/%s' % (project_path, nowtime1))
+    # if not os.path.exists(newfilepath):
+    #     os.makedirs(newfilepath)
+    sql1 = "INSERT INTO TESTINFO(HOST_NAME,HOST_IP,\
+        HOST_CREATE,CREATE_USER,CREATE_PATH)\
+        VALUES ('%s','%s','%s','%s','%s')"\
+        % (host_name,host_ip,nowtime,create_user,newfilepath)
+    print(sql1)
+    try:
+        cursor.execute(sql1)
+        if not os.path.exists(newfilepath):
+         os.makedirs(newfilepath)
+        db.commit()
+    except Exception as e:
+        db.rollback()
+        print(e)
+db.close()
